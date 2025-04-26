497cc1cf7af4ecf7baacb985bc8d1d9151d3ced2
Endrew Sousa <endrew.developer@gmail.com>
2025-04-25 22:02:03 -0300

    Começando do zero

--- requerimentos/processo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Requerimento
-
-class RequerimentoForm(forms.ModelForm):
-    class Meta:
-        model = Requerimento
-        fields = ['titulo', 'descricao', 'arquivo_anexo']
--- requerimentos/processo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from. import views
-
-urlpatterns = [
-    path('', views.lista_requerimentos, name='listas_requerimentos'),
-    path('novo/', views.novo_requerimento, name='novo_requerimento'),
-]
