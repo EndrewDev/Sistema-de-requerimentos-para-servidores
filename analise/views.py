20eb451c935a18e9183fb3eae150d75e247ef20c
Endrew Sousa <endrew.developer@gmail.com>
2025-05-01 15:14:16 -0300

    Criado o programas forms, views e urls

--- analise/views.py
+++ analise/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import HistoricoMovimentacao
+from .forms import HistoricoForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
@@ -13,4 +14,23 @@ def lista_historico(request):
     if search:
         historico = historico.filter(processo_icontains=search)
     
-    return render(request, "listas_historico.html", {'historico': historico})
+    return render(request, "listas_historicos.html", {'historico': historico})
+
+@login_required
+def historico_detalhe(request, pk):
+    historico = get_object_or_404(HistoricoMovimentacao, pk=pk)
+    return render(request, 'detalhe.html', {'detalhe': historico})
+
+@login_required
+def criar_historico(request):
+
+    if request == "POST":
+        historico_forms = HistoricoForm(request.POST)
+        if historico_forms.is_valid():
+            historico_forms.save()
+            messages.success(request, "Histórico criano com sucesso")
+            return redirect('listas-historicos')
+    else:
+        historico_forms = HistoricoForm()
+
+    return render(request, 'criar-historico.html', {'historico-criar': historico_forms})
