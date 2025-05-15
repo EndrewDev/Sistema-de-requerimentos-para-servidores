baf7e675616b396add2d51098e27cd8cf3ffd577
Endrew Sousa <endrew.developer@gmail.com>
2025-05-15 15:37:35 -0300

    Criando pasta Templates

--- core/settings.py
+++ core/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "templates/static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
